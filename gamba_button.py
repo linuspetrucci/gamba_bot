5cd1b3b0ee0a8e561d122fa6e1bd58250a354e62
Linus Petrucci <l.petrucci@unibas.ch>
2024-01-02 23:01:29 +0100

    add emoji to gamba

--- gamba_button.py
+++ gamba_button.py
@@ -2,8 +2,8 @@ import discord
 
 
 class GambaButton(discord.ui.Button):
-    def __init__(self, label, style, custom_id, custom_callback):
-        super().__init__(style=style, label=label, custom_id=custom_id)
+    def __init__(self, emoji, style, custom_id, custom_callback):
+        super().__init__(style=style, emoji=emoji, custom_id=custom_id)
         self.custom_callback = custom_callback
 
     async def callback(self, interaction: discord.Interaction):
