9944db60a885a2997b0138113f1bbd15a3d625cd
Linus Petrucci <l.petrucci@unibas.ch>
2024-01-02 21:16:55 +0100

    add reminder cog to tell people how to access new commands

---
+++ cogs/reminder_cog.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+import discord
+
+
+async def setup(bot):
+    await bot.add_cog(Reminder(bot))
+
+
+class Reminder(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.content.startswith('$'):
+            await message.channel.send('$ is deprecated, new commands are called with /')
+
+    async def cog_load(self):
+        print('Loaded utility cog')
+
+    async def cog_unload(self):
+        print('Unloaded utility cog')
